c806ece9026c129bb087905fb4b4c22d6d423c73
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-20 06:50:20 -0500

    Add Headers() action method to PocoController.cs class

--- ControllersAndActions/Controllers/PocoController.cs
+++ ControllersAndActions/Controllers/PocoController.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Linq;
 
 namespace ControllersAndActions.Controllers
 {
     public class PocoController
     {
+        [ControllerContext]
+        public ControllerContext ControllerContext { get; set; }    
+        
         public ViewResult Index() => new ViewResult() {
             ViewName = "Result",
             ViewData = new ViewDataDictionary(
@@ -14,5 +18,14 @@ namespace ControllersAndActions.Controllers
                     Model = $"This is a POCO controller"
                 }
         };
+
+        public ViewResult Headers() => 
+            new ViewResult() {
+                ViewName = "DictionaryResult",
+                ViewData = new ViewDataDictionary(
+                    new EmptyModelMetadataProvider(),
+                    new ModelStateDictionary()
+                ) { Model = ControllerContext.HttpContext.Request.Headers.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.First())}
+            };
     }
 }
