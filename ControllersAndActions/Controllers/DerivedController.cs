7afe5cf581ce142840fd2f6916dc3b89dfc80872
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-20 06:26:04 -0500

    Add initial DerivedController.cs class

---
+++ ControllersAndActions/Controllers/DerivedController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace ControllersAndActions.Controllers
+{
+    public class DerivedController : Controller 
+    {
+        public ViewResult Index() => View("Result", $"This is a derived controller");
+
+        public ViewResult Headers() => View("DictionaryResult",
+            Request.Headers.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.First()));
+    }
+}
