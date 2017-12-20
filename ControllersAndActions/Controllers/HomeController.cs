f53f4ad72279bc881afd28799daa613a89418398
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-20 06:57:34 -0500

    Add HomeController class with Index() and ReceiveForm action methods

---
+++ ControllersAndActions/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() => View("SimpleForm");
+
+        public ViewResult ReceiveForm() 
+        {
+            var name = Request.Form["name"];
+            var city = Request.Form["city"];
+            return View("Result", $"{name} lives in {city}");
+        }
+    }
+}
