67c3ef39b29f3e2a50067d4971a3ded976cfba4f
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-21 09:02:39 -0500

    Add Example.cs controller with Index action method

---
+++ ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public ViewResult Index() => View(DateTime.Now);        
+    }
+}
