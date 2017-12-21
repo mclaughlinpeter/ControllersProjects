cd93c494535b5dde347e559338198a7554fab813
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-21 07:31:37 -0500

    Add ActionTests.cs class to Tests project with ViewSelected test method

---
+++ Tests/ActionTests.cs
@@ -0,0 +1,22 @@
+using ControllersAndActions.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace ControllersAndActions.Tests
+{
+    public class ActionTests
+    {
+        [Fact]
+        public void ViewSelected()
+        {
+            // Arrange
+            HomeController controller = new HomeController(); 
+
+            // Act
+            ViewResult result = controller.ReceiveForm("Adam", "London");
+
+            // Assert
+            Assert.Equal("Result", result.ViewName);
+        }
+    }
+}
